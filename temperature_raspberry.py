831485d976ede8545a722a1d4504a4486ae3de47
Nicolas Rigaudy <nicolas.rigaudy@gmail.com>
2020-10-20 13:15:49 +0200

    Clean up git org part 2

--- temperature_raspberry.py
+++
@@ -1,9 +0,0 @@
-import time
-from w1thermsensor import W1ThermSensor
-sensor = W1ThermSensor()
-
-while True:
-    temperature = sensor.get_temperature()
-    print("La température actuelle de l'aquarium est de %s°C." % temperature)
-    time.sleep(1)
-    
--- tools.py
+++
@@ -1,6 +0,0 @@
-from datetime import datetime
-
-def dateTime():
-    now = datetime.now()
-    timeString = now.strftime("%d/%m/%Y/%H:%M:%S")
-    return timeString
